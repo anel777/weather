a2d7f6828f6d61a04e19680216efd754a37480ff
Anel <anlmc7@gmail.com>
2024-06-24 22:15:17 +0530

    3/4 working, remaining bottom and history)

--- weather/lib/weather_screen.dart
+++ weather/lib/weather_screen.dart
@@ -1,12 +1,144 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
 import 'package:weather/weather_time_tile.dart';
 
-class WeatherScreen extends StatelessWidget {
+const apiKey = '2f9215cf2add5c2c341067c93bd088ba';
+
+class WeatherScreen extends StatefulWidget {
   final bool isDarkMode;
 
   WeatherScreen({required this.isDarkMode});
+
+  @override
+  _WeatherScreenState createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  String formattedTime = DateFormat('H:mm').format(_now);
+  String formattedDay = DateFormat('E').format(_now);
+
+  String _locationLocality = '';
+  String _locationCountry = '';
+
+  String _weatherDescription = '';
+  double _temperature = 0.0;
+  int _humidity = 0;
+  double _windSpeed = 0.0;
+  List<WeatherForecast> _forecastData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _now = DateTime.now();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        _locationLocality = 'Location services are disabled.';
+      });
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _locationLocality =
+            'Location permissions are permanently denied, we cannot request permissions.';
+      });
+      return;
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        setState(() {
+          _locationLocality =
+              'Location permissions are denied (actual value: $permission).';
+        });
+        return;
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark placemark = placemarks[0];
+
+    setState(() {
+      _locationLocality = '${placemark.locality}';
+      _locationCountry = '${placemark.country}';
+    });
+
+    _fetchWeather(position.latitude, position.longitude);
+  }
+
+  Future<void> _fetchWeather(double lat, double lon) async {
+    final url =
+        'http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        _weatherDescription = data['weather'][0]['main'];
+        _temperature = data['main']['temp'];
+        _humidity = data['main']['humidity'];
+        _windSpeed = data['wind']['speed'];
+      });
+    } else {
+      setState(() {
+        _locationLocality = 'Failed to load weather data.';
+      });
+    }
+
+    _fetchForecast(lat, lon);
+  }
+
+  Future<void> _fetchForecast(double lat, double lon) async {
+    final url =
+        'http://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&appid=$apiKey&units=metric';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body)['list'] as List<dynamic>;
+      List<WeatherForecast> forecasts = [];
+
+      for (var item in data) {
+        forecasts.add(WeatherForecast(
+          time: item['dt_txt'].toString().substring(11, 16),
+          temperature: item['main']['temp'].toString(),
+          humidity: item['main']['humidity'].toString(),
+          wind: item['wind']['speed'].toString(),
+          weather: item['weather'][0]['main'].toString(),
+        ));
+      }
+
+      setState(() {
+        _forecastData = forecasts;
+      });
+    } else {
+      setState(() {
+        _locationLocality = 'Failed to load forecast data.';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    String _locationCont = _locationCountry.substring(0, 2);
     double screenRatio = MediaQuery.of(context).size.aspectRatio;
     return Column(
       children: [
@@ -19,9 +151,11 @@ class WeatherScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('IN | Kottakkal', style: TextStyle(fontSize: 30)),
                     Text(
-                      'Scattered Clouds',
+                        '${_locationCont.toUpperCase()} | ${_locationLocality}',
+                        style: TextStyle(fontSize: 30)),
+                    Text(
+                      _weatherDescription,
                       style: TextStyle(fontSize: 12),
                     ),
                   ],
@@ -31,9 +165,10 @@ class WeatherScreen extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text('34°', style: TextStyle(fontSize: screenRatio * 160)),
+                  Text('${_temperature.round()}°',
+                      style: TextStyle(fontSize: screenRatio * 160)),
                   Icon(
-                    Icons.cloud,
+                    _getWeatherIcon(_weatherDescription),
                     size: screenRatio * 165,
                   ),
                 ],
@@ -42,7 +177,8 @@ class WeatherScreen extends StatelessWidget {
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(screenRatio * 40),
-                  color: isDarkMode ? Color(0xFF23282B) : Color(0xFFB0BCC8),
+                  color:
+                      widget.isDarkMode ? Color(0xFF23282B) : Color(0xFFB0BCC8),
                 ),
                 padding: EdgeInsets.all(screenRatio * 60),
                 child: Row(
@@ -51,14 +187,14 @@ class WeatherScreen extends StatelessWidget {
                     Column(
                       children: [
                         Icon(Icons.thermostat, size: 50),
-                        Text('41°'),
+                        Text('${_temperature.round()}°'),
                       ],
                     ),
                     Column(
                       children: [
                         Icon(Icons.water_drop_outlined, size: 80),
                         Text(
-                          '49%',
+                          '$_humidity%',
                           style: TextStyle(fontSize: 24),
                         ),
                       ],
@@ -66,7 +202,7 @@ class WeatherScreen extends StatelessWidget {
                     Column(
                       children: [
                         Icon(Icons.air, size: 50),
-                        Text('3 km/h'),
+                        Text('$_windSpeed km/h'),
                       ],
                     ),
                   ],
@@ -89,43 +225,16 @@ class WeatherScreen extends StatelessWidget {
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
-              children: [
-                WeatherTimeTile(
-                  time: '09:00 AM',
-                  temperature: '38°',
-                  prob: '57%',
-                  wind: '5km/h',
-                  isDarkMode: isDarkMode,
-                ),
-                WeatherTimeTile(
-                  time: '12:00 PM',
-                  temperature: '33°',
-                  prob: '59%',
-                  wind: '9km/h',
-                  isDarkMode: isDarkMode,
-                ),
-                WeatherTimeTile(
-                  time: '04:00 PM',
-                  temperature: '34°',
-                  prob: '52%',
-                  wind: '2km/h',
-                  isDarkMode: isDarkMode,
-                ),
-                WeatherTimeTile(
-                  time: '06:00 PM',
-                  temperature: '34°',
-                  prob: '46%',
-                  wind: '14km/h',
-                  isDarkMode: isDarkMode,
-                ),
-                WeatherTimeTile(
-                  time: '09:00 PM',
-                  temperature: '34°',
-                  prob: '29%',
-                  wind: '2km/h',
-                  isDarkMode: isDarkMode,
-                ),
-              ],
+              children: _forecastData
+                  .map((forecast) => WeatherTimeTile(
+                        time: forecast.time,
+                        temperature: forecast.temperature,
+                        hum: forecast.humidity,
+                        wind: forecast.wind,
+                        weather: forecast.weather,
+                        isDarkMode: widget.isDarkMode,
+                      ))
+                  .toList(),
             ),
           ),
         ),
@@ -142,7 +251,7 @@ class WeatherScreen extends StatelessWidget {
         Container(
           height: 50,
           decoration: BoxDecoration(
-            color: isDarkMode ? Color(0xFF1E1F21) : Color(0xFFB0BCC8),
+            color: widget.isDarkMode ? Color(0xFF1E1F21) : Color(0xFFB0BCC8),
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(20.0),
               topRight: Radius.circular(20.0),
@@ -150,20 +259,63 @@ class WeatherScreen extends StatelessWidget {
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  Text('Wed'),
-                  Text('9 AM'),
-                ],
-              ),
-              Icon(Icons.cloudy_snowing),
-              Text('26'),
-              Text('5km/h')
-            ],
+            children: [Text('Wed'), Icon(Icons.cloud), Text('26°C')],
           ),
         ),
       ],
     );
   }
+
+  IconData _getWeatherIcon(String weatherDescription) {
+    switch (weatherDescription.toLowerCase()) {
+      case 'thunderstorm':
+        return Icons.cloud_circle;
+      case 'drizzle':
+        return Icons.grain;
+      case 'rain':
+        return Icons.beach_access;
+      case 'snow':
+        return Icons.ac_unit;
+      case 'mist':
+        return Icons.cloud_circle;
+      case 'smoke':
+        return Icons.cloud_circle;
+      case 'haze':
+        return Icons.cloud_circle;
+      case 'dust':
+        return Icons.cloud_circle;
+      case 'fog':
+        return Icons.cloud_circle;
+      case 'sand':
+        return Icons.cloud_circle;
+      case 'ash':
+        return Icons.cloud_circle;
+      case 'squall':
+        return Icons.cloud_circle;
+      case 'tornado':
+        return Icons.cloud_circle;
+      case 'clear':
+        return Icons.wb_sunny;
+      case 'clouds':
+        return Icons.cloud;
+      default:
+        return Icons.cloud;
+    }
+  }
+}
+
+class WeatherForecast {
+  final String time;
+  final String temperature;
+  final String wind;
+  final String humidity;
+  final String weather;
+
+  WeatherForecast({
+    required this.time,
+    required this.temperature,
+    required this.humidity,
+    required this.wind,
+    required this.weather,
+  });
 }
--- weather/lib/weather_time_tile.dart
+++ weather/lib/weather_time_tile.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 class WeatherTimeTile extends StatelessWidget {
   final String time;
   final String temperature;
-  final String prob;
+  final String hum;
   final String wind;
+  final String weather;
   final bool isDarkMode;
 
   const WeatherTimeTile({
     required this.time,
     required this.temperature,
-    required this.prob,
+    required this.hum,
     required this.wind,
+    required this.weather,
     required this.isDarkMode,
   });
 
@@ -37,7 +39,7 @@ class WeatherTimeTile extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Text(temperature, style: TextStyle(fontSize: 24)), //Temp
-                Icon(Icons.cloud, size: 32), //Cloud Icon
+                Icon(_getWeatherIcon(weather), size: 32), //Cloud Icon
               ],
             ),
             Row(
@@ -47,25 +49,62 @@ class WeatherTimeTile extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Icon(Icons.water_drop),
-                    Text(wind),
+                    Text(hum),
                   ],
                 ),
                 Column(
-                  //Raining probability
+                  //Humidity
                   children: [
-                    Icon(Icons.air_rounded),
-                    Text(prob),
+                    Icon(Icons.air),
+                    Text(wind),
                   ],
                 ),
               ],
             ),
             Text(
-              'Scattered Clouds',
+              '$weather',
               style: TextStyle(fontSize: 10),
             )
           ],
         ),
       ),
     );
+  }
+}
+
+IconData _getWeatherIcon(String weatherDescription) {
+  switch (weatherDescription.toLowerCase()) {
+    case 'thunderstorm':
+      return Icons.cloud_circle;
+    case 'drizzle':
+      return Icons.grain;
+    case 'rain':
+      return Icons.beach_access;
+    case 'snow':
+      return Icons.ac_unit;
+    case 'mist':
+      return Icons.cloud_circle;
+    case 'smoke':
+      return Icons.cloud_circle;
+    case 'haze':
+      return Icons.cloud_circle;
+    case 'dust':
+      return Icons.cloud_circle;
+    case 'fog':
+      return Icons.cloud_circle;
+    case 'sand':
+      return Icons.cloud_circle;
+    case 'ash':
+      return Icons.cloud_circle;
+    case 'squall':
+      return Icons.cloud_circle;
+    case 'tornado':
+      return Icons.cloud_circle;
+    case 'clear':
+      return Icons.wb_sunny;
+    case 'clouds':
+      return Icons.cloud;
+    default:
+      return Icons.cloud;
   }
 }
